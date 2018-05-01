54428f569eec20d8b9155f0579181a7be7a34844
James Townsend <j.a.townsend2000@gmail.com>
2018-05-01 15:26:54 +0100

    Player Presence
    Added a player of sorts to the game. It has no effect on other mechanics, but is more of a test as to an implementation of one in my coursework.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,9 +6,17 @@ public class CameraController : MonoBehaviour {
 
     public float cameraSpeed;
 
-	// Update is called once per frame
-	void Update () {
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
         float hzAxis = Input.GetAxis("Horizontal");
-        gameObject.transform.Translate(new Vector3(hzAxis * Time.deltaTime * cameraSpeed, 0, 0));
+        //gameObject.transform.Translate(new Vector3(hzAxis * Time.deltaTime * cameraSpeed, 0, 0)); // Deprecated code
+        gameObject.transform.position = new Vector3(player.position.x, 0, -10);
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpheight;
+
+    private float hzaxis;
+    private float jump;
+    private Rigidbody2D rb;
+    private int jumpcountdown;
+
+    // Use this for initialization
+    void Start () {
+        jumpcountdown = 0;
+        rb = GetComponent<Rigidbody2D>(); // Get the rigidbody of the palyer
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        jumpcountdown--; // Countdown the jump countdown
+        hzaxis = Input.GetAxis("Horizontal");
+        jump = Input.GetAxis("Jump"); // Get control axes
+        transform.rotation = Quaternion.Euler(0, 0, 0); // Enforce zero rotation
+        rb.AddForce(new Vector2(hzaxis * speed, 0));  // Propel the player in their desired direction of motion
+	}
+
+    private void OnCollisionStay2D(Collision2D collision) // If they are colliding with a tile
+    {
+        if (jump > 0 && jumpcountdown < 0) // If they press jump and they have not jumped in the last 5 physics updates
+        {
+            rb.AddForce(new Vector2(0, jumpheight)); // Jump
+            jumpcountdown = 5; // Set a countdown (this stops huge launches into the air)
+        }
+    }
+}
